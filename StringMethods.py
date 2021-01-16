be8eb7bbbf3cfad787e75542ba558d3aae19e805
anil nainegali <anilnainegali@anils-MacBook-Pro.local>
2021-01-17 00:46:23 +0800

    InitialCommit

---
+++ StringMethods.py
@@ -0,0 +1,12 @@
+#Testing all string methods
+#dir("String") #Call from Terminal
+y='Water is cold'
+
+print(y)
+print(y[2])
+print(y.replace('t', 'a'))
+print(y[0:2])
+print(y[-2:])
+print(y[3:])
+print(y.upper())
+print(y.rsplit(" "))
